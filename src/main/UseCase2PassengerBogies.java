0f6e2be8e9f8019a36d22fe30882fc9560891db8
Yaswanth Reddy Papari <papariyaswanthreddy@gmail.com>
2026-03-30 20:12:07 +0530

    UC2: Add passenger bogies using ArrayList operations

---
+++ src/main/UseCase2PassengerBogies.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UseCase2PassengerBogies {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println("UC2 - Add Passenger Bogies to Train");
+        System.out.println("=====================================\n");
+
+        // Create ArrayList
+        List<String> passengerBogies = new ArrayList<>();
+
+        // ADD bogies
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("After Adding Bogies:");
+        System.out.println("Passenger Bogies: " + passengerBogies);
+
+        // REMOVE one bogie
+        passengerBogies.remove("AC Chair");
+
+        System.out.println("\nAfter Removing 'AC Chair':");
+        System.out.println("Passenger Bogies: " + passengerBogies);
+
+        // CHECK existence
+        System.out.println("\nChecking if 'Sleeper' exists:");
+        System.out.println("Contains Sleeper? : " + passengerBogies.contains("Sleeper"));
+
+        // FINAL OUTPUT
+        System.out.println("\nFinal Train Passenger Consist:");
+        System.out.println(passengerBogies);
+
+        System.out.println("\nUC2 operations completed successfully...");
+    }
+}
