7947c516c9cd5d2c781f90e98af8bf2c6494f46c
Yaswanth Reddy Papari <papariyaswanthreddy@gmail.com>
2026-04-24 15:55:12 +0530

    UC16: Sort passenger bogies using Bubble Sort algorithm

---
+++ src/main/UseCase16BubbleSort.java
@@ -0,0 +1,41 @@
+package main;
+
+public class UseCase16BubbleSort {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println("UC16 - Manual Sorting using Bubble Sort");
+        System.out.println("=====================================\n");
+
+        // Create array of capacities
+        int[] capacities = {72, 56, 24, 70, 60};
+
+        // Display original
+        System.out.println("Original Capacities:");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
+
+        // -------- BUBBLE SORT --------
+        for (int i = 0; i < capacities.length - 1; i++) {
+
+            for (int j = 0; j < capacities.length - i - 1; j++) {
+
+                if (capacities[j] > capacities[j + 1]) {
+
+                    // swap
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
+            }
+        }
+
+        // Display sorted
+        System.out.println("\n\nSorted Capacities (Ascending):");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
+    }
+}
